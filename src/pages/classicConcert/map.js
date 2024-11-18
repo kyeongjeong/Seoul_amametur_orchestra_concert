531a478a756b5c93f6f57f144906bcbf8f5c1a9b
ohyeong <oueong@naver.com>
2024-11-19 02:26:46 +0900

    Refactor: 지도 API 자바스크립트 코드에서 데이터 부분을 json 파일로 나눔

--- src/pages/classicConcert/map.js
+++ src/pages/classicConcert/map.js
@@ -6,92 +6,72 @@ mapOption = {
 
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
-// 마커위치
-var positions = [
-{
-    title: '광운대학교',
-    latLng: new kakao.maps.LatLng(37.619247, 127.058271),
-    iwRemoveable: false,
-    content:  '<div class="wrap">' + 
-        '    <div class="info">' + 
-        '        <div class="title">' + 
-        '            공연이름' + 
-        '            <div class="close" onclick="closeOverlay(' + 0 + ')" title="닫기"></div>' + 
-        '        </div>' + 
-        '        <div class="body">' + 
-        '            <div class="img">' +
-        '                <img src="/public/images/kwangwoon.jpg" width="73" height="70">' +
-        '           </div>' + 
-        '            <div class="desc">' + 
-        '                <div class="ellipsis">서울 노원구 광운로 20</div>' + 
-        '                <div class="jibun ellipsis">날짜' + 
-        '                <div><a href="https://www.kw.ac.kr/ko/" target="_blank" class="link">홈페이지</a></div>' + 
-        '            </div>' + 
-        '        </div>' + 
-        '    </div>' +    
-        '</div>'
-},
-{
-    title: '경희대학교',
-    latLng: new kakao.maps.LatLng(37.597173, 127.051746),
-    iwRemoveable: false,
-    content:  '<div class="wrap">' + 
-        '    <div class="info">' + 
-        '        <div class="title">' + 
-        '            경희대학교' + 
-        '            <div class="close" onclick="closeOverlay(' + 1 + ')" title="닫기"></div>' + 
-        '        </div>' + 
-        '        <div class="body">' + 
-        '            <div class="img">' +
-        '                <img src="/public/images/kyunghee.jpg" width="73" height="70">' +
-        '           </div>' + 
-        '            <div class="desc">' + 
-        '                <div class="ellipsis">서울 동대문구 경희대로 26</div>' + 
-        '                <div class="jibun ellipsis">(우) 02447 (지번) 회기동 1-5</div>' + 
-        '                <div><a href="http://www.khu.ac.kr/" target="_blank" class="link">홈페이지</a></div>' + 
-        '            </div>' + 
-        '        </div>' + 
-        '    </div>' +    
-        '</div>'
-}
-];
-
 var overlays = []; // 열린 오버레이를 저장할 배열
 
-// 마커와 커스텀 오버레이 생성
-positions.forEach(function(position) {
-var marker = new kakao.maps.Marker({
-    map: map,
-    position: position.latLng
-});
+// JSON 데이터 로드
+fetch('positions.json') // JSON 파일 경로
+    .then(response => response.json())
+    .then(positions => {
+        // 마커와 커스텀 오버레이 생성
+        positions.forEach((position, index) => {
+            // 마커 생성
+            var marker = new kakao.maps.Marker({
+                map: map,
+                position: new kakao.maps.LatLng(position.latLng.lat, position.latLng.lng)
+            });
 
-var overlay = new kakao.maps.CustomOverlay({
-    content: position.content,
-    position: position.latLng,
-    map: null // 처음에는 보이지 않도록 설정
-});
+            // 커스텀 오버레이 콘텐츠 생성
+            var content = `
+                <div class="wrap">
+                    <div class="info">
+                        <div class="title">
+                            ${position.title}
+                            <div class="close" onclick="closeOverlay(${index})" title="닫기"></div>
+                        </div>
+                        <div class="body">
+                            <div class="img">
+                                <img src="${position.image}" width="73" height="70">
+                            </div>
+                            <div class="desc">
+                                <div class="ellipsis">${position.address}</div>
+                                ${position.date ? `<div class="jibun ellipsis">${position.date}</div>` : ""}
+                                ${position.postal ? `<div class="jibun ellipsis">${position.postal}</div>` : ""}
+                                <div><a href="${position.link}" target="_blank" class="link">홈페이지</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
 
-overlays.push(overlay);
+            // 오버레이 생성
+            var overlay = new kakao.maps.CustomOverlay({
+                content: content,
+                position: new kakao.maps.LatLng(position.latLng.lat, position.latLng.lng),
+                map: null // 처음에는 보이지 않도록 설정
+            });
 
-// 마커 클릭 시 오버레이 표시/닫기
-kakao.maps.event.addListener(marker, 'click', function() {
-    if (overlay.getMap()) {
-        overlay.setMap(null); // 오버레이가 열려 있다면 닫기
-    } else {
-        closeAllOverlays(); // 모든 오버레이 닫기
-        overlay.setMap(map); // 해당 오버레이 표시
-    }
-});
-});
+            overlays.push(overlay);
+
+            // 마커 클릭 시 오버레이 표시/닫기
+            kakao.maps.event.addListener(marker, 'click', function() {
+                if (overlay.getMap()) {
+                    overlay.setMap(null); // 오버레이가 열려 있다면 닫기
+                } else {
+                    closeAllOverlays(); // 모든 오버레이 닫기
+                    overlay.setMap(map); // 해당 오버레이 표시
+                }
+            });
+        });
+    });
 
 // 모든 오버레이 닫기
 function closeAllOverlays() {
-overlays.forEach(function(overlay) {
-    overlay.setMap(null);
-});
+    overlays.forEach(function(overlay) {
+        overlay.setMap(null);
+    });
 }
 
 // 특정 오버레이 닫기
 function closeOverlay(index) {
-overlays[index].setMap(null);
+    overlays[index].setMap(null);
 }
